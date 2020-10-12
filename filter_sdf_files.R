dd1ed451f2ce7c3b1f98a0e20862e7cb78cef3e4
AhmedElsherbini <49863685+AhmedElsherbini@users.noreply.github.com>
2020-10-13 01:58:41 +0200

    Update filter_sdf_files.R

--- filter_sdf_files.R
+++ filter_sdf_files.R
@@ -1,5 +1,6 @@
 #@uthor: Ahmed Elsherbini - 26-08-2020
 #mail:drahmedsherbini@yahoo.com
+#please cite my page if you used this script
 
 #install your packages
 
@@ -44,9 +45,10 @@ write_sdf(STAPH_MIX,"filtered_file_for_ecoli.sdf")
 
 
 
-#lets cluster our SDF database 
+#lets cluster our filtered SDF to see how much they are similar
 #there was a probelm in installing ChemmineR and FmcsR due to a bug in rvsg package
 #to install package ("rsvg") in Ubunto 20.04 do not use CRAN repo but in your linux terminal type $ sudo apt-get install -y librsvg2-dev
+#install the packages now without any hassle now
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 BiocManager::install("ChemmineR")
@@ -64,7 +66,7 @@ simMA <- sapply(cid(fpset), function(x) fpSim(fpset[x], fpset, sorted=FALSE))
 hc <- hclust(as.dist(1-simMA), method="single") 
 plot(as.dendrogram(hc), edgePar=list(col=4, lwd=2), horiz=TRUE) 
 
-#plot your heatmap
+#plot your heatmap (more clear than the dendogram)
 heatmap.2(1-distmat, Rowv=as.dendrogram(hc), Colv=as.dendrogram(hc), 
           col=colorpanel(40, "darkblue", "yellow", "white"), 
           density.info="none", trace="none") 
