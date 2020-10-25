51af37a4a884432f205c3c166cdb91b25ea22ebd
AhmedElsherbini <49863685+AhmedElsherbini@users.noreply.github.com>
2020-10-25 19:00:05 +0200

    Update filter_sdf_files.R

--- filter_sdf_files.R
+++ filter_sdf_files.R
@@ -1,32 +1,34 @@
 #@uthor: Ahmed Elsherbini - 26-08-2020
 #mail:drahmedsherbini@yahoo.com
-#please cite my page if you used this script
-
+#update: 25/10/2020
 #install your packages
-
+#####################################################################################
+#first part - filter and deduplicate your sdf based on metadata (example:organisms)
 install.packages("cinf", repos="http://R-Forge.R-project.org")
 install.packages("conmolfields", repos="http://R-Forge.R-project.org")
 install.packages("rlist",repos="http://R-Forge.R-project.org")
-install.packages("gplots",repos="http://R-Forge.R-project.org")
-
+#####################################################################################
 
 #load your libraries
 
 library(conmolfields)
 library("cinf")
 library(rlist)
+library(gplots) 
+library(rlist)
 
 # let's us read our files 
 #put here your downladed
-mdb = read_sdf("YOUR_PATH_TO_THE_FILE/Your_mixed_file.sdf")
+setwd("path to you dir")
+#read your data
+mdb = read_sdf("path to your file.sdf")
 
-#make two empty list for the two separte categeori you want to divide your files to, if you have 3 categories , make a three empty list
-#make the names of the empty as you like #here I made it upon the organism
+#make an empty list 
+
 ECOLI_MIX = list()
 STAPH_MIX = list()
 
-#here to loop over the main SDF file, then filter it according to a certain chosen parameter (= organism) 
-# if you want another paramter just type after prop$ and see which paramter you want to divide you data for. 
+#here to loop over the main SDF file, then filter it according to a certain chosen parameter (= organism, s.aureus &eE.coli) 
 #here also, I use Unique() to remove any duplicates
 
 for (x in mdb) { 
@@ -38,35 +40,61 @@ for (x in mdb) {
 }
 
 
-#export the data into two sdf files one for ecoli and and one for staph
-#change the name as you like
-write_sdf(ECOLI_MIX,"filtered_file_for_staph.sdf") 
-write_sdf(STAPH_MIX,"filtered_file_for_ecoli.sdf")
+#export the data into sdf file
+write_sdf(ECOLI_MIX,"example_1.sdf")
+write_sdf(STAPH_MIX,"example_2.sdf")
 
-
-
-#lets cluster our filtered SDF to see how much they are similar
-#there was a probelm in installing ChemmineR and FmcsR due to a bug in rvsg package
-#to install package ("rsvg") in Ubunto 18.04 or 20.04 do not use CRAN repo but, in your linux terminal type $ sudo apt-get install -y librsvg2-dev
-#install the packages now without any hassle now
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
+###################################################################################################################### 
+#Second-part- cluster your sdf based on chemical fingerprint.
+#to install package ("rsvg") in Ubunto 20.04 #in your linux terminal type $ sudo apt-get install -y librsvg2-dev (do not use cran repo)
 BiocManager::install("ChemmineR")
 BiocManager::install("fmcsR")
-sdfset <- read.SDFset("your_Path/file.sdf") #change the path accprding 
-apset <- sdf2ap(sdfset) #we need to convert sdf to atom pair dataset
-fpset <- desc2fp(apset) #we need to convert ato,
+#install your libriaries
+library("ChemmineR") # Loads the 
+library("fmcsR")
+library("gplots")
 
-#creat the distanse between your compunds and save it is .rda file
-dummy <- cmp.cluster(db=apset, cutoff=0, save.distances="distmat.rda", quiet=TRUE) #lets make the 
+#you need to reread the data with sdfset 
+sdfset = read.SDFset("/media/ahmed/CC69-620B/0_downloads/dr.tamer_FQ_/new_project/sepated_sdf/staph_gyra_30.sdf")
+#d = sapply(cid(sdfset), function(x) fmcsBatch(sdfset[x], sdfset, au=0, bu=0, matching.mode="aromatic")[,"Overlap_Coefficient"])
+#hc = hclust(as.dist(1-d), method="complete")
+#plot(as.dendrogram(hc), edgePar=list(col=4, lwd=2), horiz=TRUE)
+
+apset = sdf2ap(sdfset)
+fpset = desc2fp(apset)
+
+#
+dummy = cmp.cluster(db=apset, cutoff=0, save.distances="distmat.rda", quiet=TRUE) 
 load("distmat.rda") 
 
-#plot your dendogram
-simMA <- sapply(cid(fpset), function(x) fpSim(fpset[x], fpset, sorted=FALSE))
-hc <- hclust(as.dist(1-simMA), method="single") 
-plot(as.dendrogram(hc), edgePar=list(col=4, lwd=2), horiz=TRUE) 
-
-#plot your heatmap (more clear than the dendogram)
+#fingerptting/heirchal clusterng/bulid a heatmap
+simMA = sapply(cid(fpset), function(x) fpSim(fpset[x], fpset, sorted=FALSE))
+hc = hclust(as.dist(1-simMA), method="single") 
+plot(as.dendrogram(hc), edgePar=list(col=4, lwd=2), horiz=TRUE)
+pdf("nameofyourplot.pdf") 
 heatmap.2(1-distmat, Rowv=as.dendrogram(hc), Colv=as.dendrogram(hc), 
           col=colorpanel(40, "darkblue", "yellow", "white"), 
-          density.info="none", trace="none") 
+          density.info="none", trace="none") #white similar-blue dissmilar
+dev.off()
+#open the pdf file and copy the numbers and paste it in a list
+#to convert a column to comma separated list use (https://convert.town/column-to-comma-separated-list) 
+###############################################################################################################
+#third part- subset your sdf based on id/headers
+               
+mdb = read_sdf("path_to your.sdf")
+#here for example I want to extract the 12th and 3rd and the 134th compound from my sdf file or the comma seperated list from the above chunck
+list = list(12,3,134)
+subset = list()
+for (x in list){
+  subset = unique(c(subset,mdb[x]))
+}
+write_sdf(subset,"subset.sdf")
+###################################################################################################################
+#the fourth part to print ID list of your sdf files                
+id_list = list()
+
+for (x in mdb){
+  id_list= c(id_list,list(x$props$`BindingDB Ligand Name`))
+}
+print(id_list)
+
